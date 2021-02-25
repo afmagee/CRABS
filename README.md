1bb630ff80dcfba869624ef87e5728331cfc167a
Sebastian Hoehna <sebastian.hoehna@gmail.com>
2021-02-24 18:11:03 -0600

    started restructuering

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # shiny-congruence-classes
 An Rshiny app to vizualize congruence classes
+
+# Installation
+
+At present, XXX is downloadable from https://github.com/XXX. There are multiple ways to download it. The easiest is to use devtools and install from GitHub.
+
+### Installing from GitHub using devtools
+Run the following code from your R console:
+
+```{r eval=FALSE}
+install.packages("devtools")
+library(devtools)
+install_github("XXX")
+library(XXX)
+```
+
+# Manual and Tutorials
+
+All functions in XXX are documented according to the CRAN standard and the manual can be downloaded from https://raw.githubusercontent.com/XXX
+
+
+# Citations
+
+The idea and methods behind XXX has been published in several papers, listed below. The main paper about XXX is the application note in Bioinformatics. Please cite the software and papers according to your type of analysis.
+
+* Höhna, S. XXX
---
+++ src/old/utils.R
@@ -0,0 +1,32 @@
+###  create the pulled diversification rate
+compute.pulled.diversification <- function( v_spec0, v_ext0, times ) {
+
+  # compute the derivatives
+  l            <- v_spec0[-length(times)]
+  l_plus_one   <- v_spec0[-1]
+  l_derivative <- (l_plus_one - l) / delta_t
+  l_derivative <- c(l_derivative[1],l_derivative)
+
+  # finally, add the 1/lambda * lambda dt to the pulled diversification rate
+  v_p_div <- v_spec0 - v_ext0 + 1/v_spec0 * l_derivative
+
+  return (v_p_div)
+}
+
+
+
+compute.speciation <- function( lambda0, v_p_div, v_ext1, delta_t ) {
+
+  ### compute the new lambda
+  v_lambda1    <- c()
+  v_lambda1[1] <- lambda0
+
+  for (j in 2:(NUM_TIME_DISCRETIZATIONS+1)) {
+
+	tmp <- 4*v_lambda1[j-1]*delta_t + (v_p_div[j]*delta_t+v_ext1[j]*delta_t-1)^2
+	v_lambda1[j] <- (sqrt(tmp) + v_p_div[j]*delta_t+v_ext1[j]*delta_t-1) / (2*delta_t)
+
+  }
+
+  return (v_lambda1)
+}
