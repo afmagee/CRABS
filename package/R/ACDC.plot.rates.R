f024d1df846d9a9516f8382c252f8d98c4874bb0
Sebastian Hoehna <sebastian.hoehna@gmail.com>
2021-03-03 08:24:29 -0600

    better plots of congruence class

--- package/R/ACDC.plot.rates.R
+++ package/R/ACDC.plot.rates.R
@@ -14,9 +14,9 @@ ACDC.plot.rates <- function( obj ) {
   ## plot settings
   col_lambda    = "darkblue"
   col_mu        = "red3"
-  col_delta     = "green"
-  col_epsilon   = "purple"
-  this.lwd      = 5
+  col_delta     = "darkorchid4"
+  col_epsilon   = "darkgreen"
+  this.lwd      = 3
 
   ## extract the rate functions
   lambda        = obj$lambda
@@ -30,21 +30,36 @@ ACDC.plot.rates <- function( obj ) {
 
   Y_MIN <- min(lambda(times), p.lambda(times))
   Y_MAX <- max(lambda(times), p.lambda(times))
-  curve(lambda, xlim=c(0,obj$max.t), ylim=c(Y_MIN,Y_MAX), lwd=this.lwd, col=col_lambda, ylab="rate", xlab="time before present", lty=1)
+  curve(lambda, xlim=rev(c(0,obj$max.t)), ylim=c(Y_MIN,Y_MAX), lwd=this.lwd, col=col_lambda, ylab="", xlab="", main="", lty=1)
   curve(p.lambda, lwd=this.lwd, col=col_lambda, lty=2, add=TRUE)
+  mtext(side=1, text="time before present", line=2.5, cex=1.25)
+  mtext(side=2, text="rate", line=2.25, cex=1.25)
+  mtext(side=3, text="Speciation", line=0.75, cex=1.5)
+
 
   Y_MIN <- min(mu(times))
   Y_MAX <- max(mu(times))
-  curve(mu, xlim=c(0,obj$max.t), ylim=c(Y_MIN,Y_MAX), lwd=this.lwd, col=col_mu, ylab="rate", xlab="time before present", lty=1)
+  curve(mu, xlim=rev(c(0,obj$max.t)), ylim=c(Y_MIN,Y_MAX), lwd=this.lwd, col=col_mu, ylab="", xlab="", main="", lty=1)
+  mtext(side=1, text="time before present", line=2.5, cex=1.25)
+  mtext(side=2, text="rate", line=2.25, cex=1.25)
+  mtext(side=3, text="Extinction", line=0.75, cex=1.5)
+
 
   Y_MIN <- min(delta(times), p.delta(times))
   Y_MAX <- max(delta(times), p.delta(times))
-  curve(delta, xlim=c(0,obj$max.t), ylim=c(Y_MIN,Y_MAX), lwd=this.lwd, col=col_delta, ylab="rate", xlab="time before present", lty=1)
+  curve(delta, xlim=rev(c(0,obj$max.t)), ylim=c(Y_MIN,Y_MAX), lwd=this.lwd, col=col_delta, ylab="", xlab="", main="", lty=1)
   curve(p.delta, lwd=this.lwd, col=col_delta, lty=2, add=TRUE)
+  mtext(side=1, text="time before present", line=2.5, cex=1.25)
+  mtext(side=2, text="rate", line=2.25, cex=1.25)
+  mtext(side=3, text="Net-diversification", line=0.75, cex=1.5)
+
 
   Y_MIN <- min(epsilon(times))
   Y_MAX <- max(epsilon(times))
-  curve(epsilon, xlim=c(0,obj$max.t), ylim=c(Y_MIN,Y_MAX), lwd=this.lwd, col=col_epsilon, ylab="rate", xlab="time before present", lty=1)
+  curve(epsilon, xlim=rev(c(0,obj$max.t)), ylim=c(Y_MIN,Y_MAX), lwd=this.lwd, col=col_epsilon, ylab="", xlab="", main="", lty=1)
+  mtext(side=1, text="time before present", line=2.5, cex=1.25)
+  mtext(side=2, text="rate", line=2.25, cex=1.25)
+  mtext(side=3, text="Relative extinction", line=0.75, cex=1.5)
 
 
 
