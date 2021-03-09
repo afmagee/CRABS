d4df215f8a80914e76d5f57a70a469b4109a0c74
Andrew Magee <afmagee@uw.edu>
2021-03-08 21:59:06 -0800

    Showcasing sample.basic.models

--- example_new_rate_simulator.R
+++ example_new_rate_simulator.R
@@ -8,8 +8,8 @@ library(ACDC)
 # source("src/plot.congruence.class.R")
 #source("src/nonparametric.pulled.diversification.R")
 
-
-tree <- primates
+data("primates")
+tree <- primates$phy
 
 times <- sort( as.numeric( branching.times( tree ) ) )
 
@@ -34,13 +34,13 @@ ylim <- c(min(0,est_speciation,est_extinction),max(est_speciation,est_extinction
 
 curve( lambda, xlim=c(0, max_t), lwd=2, lty=1, col="blue", ylim=ylim, main="", xlab="time", ylab="rate", cex.main=1.5, cex.lab=1.5 )
 curve( mu, add=TRUE, lwd=2, lty=1, col="red" )
-legend("topleft",legend=c("speciation","extinction"),col=c("blue","red"), lty=c(1,1), cex=1.2,border=NA,bty="n")
+legend("topright",legend=c("speciation","extinction"),col=c("blue","red"), lty=c(1,1), cex=1.2,border=NA,bty="n")
 
 est_lambda0 = est_speciation[1]
 est_mu0     = est_extinction[1]
 
 speciation_rate_samples <- function() { sample.basic.models( num.epochs=100, rate0=est_lambda0, max.rate=2, model="episodic2", fc.mean=1.5) }
-extinction_rate_samples <- function() { sample.basic.models( num.epochs=100, max.rate=2, model="exponential", fc.mean=2, noisy=TRUE) }
+extinction_rate_samples <- function() { sample.basic.models( num.epochs=100, max.rate=1, direction="decrease", noisy=TRUE, monotonic=TRUE, model="episodic5",fc.mean=4,rate0.median=0.05) }
 
 # speciation_rate_samples <- function() { bootstrap.rates(1000, lambda, max_t, keep.rate0=TRUE, replace=FALSE, block.size=100) }
 # extinction_rate_samples <- function() { bootstrap.rates(1000, lambda, max_t, keep.rate0=FALSE, reverse.time=TRUE, replace=TRUE, block.size=5) }
@@ -52,4 +52,5 @@ samples <- sample.congruence.class(func_spec0=lambda, func_ext0=mu, max.t=max_t,
 pdf("~/Downloads/congruence_spaghetti.pdf")
   ACDC.plot.congruence.class.spaghetti(func_spec0=lambda, func_ext0=mu, max.t=max_t, sample.grid=samples )
 dev.off()
+
 
