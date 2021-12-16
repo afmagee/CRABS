fd8468a5a829d744c28e664a29a879822144178a
Bjorn Tore Kopperud <kopperud@protonmail.com>
2021-12-16 18:01:10 +0100

    add additional birth-death model estimates as data

--- R/data.R
+++ R/data.R
@@ -9,7 +9,7 @@
 "primates"
 
 
-#' Primates birth-death model
+#' RevBayes Primates birth-death model
 #' 
 #' The results of a bayesian horseshoe markov random field (HSMRF) episodic birth-death model, fitted on the primates tree. One hundred episodes. Each estimate is the posterior median. The time unit is millions of years before the present.
 #' 
@@ -28,3 +28,24 @@
 #' @usage data(primates_ebd_log)
 #' 
 "primates_ebd_log"
+
+
+#' TESS Primates birth-death model
+#' 
+#' The results of a bayesian episodic birth-death model in the R-package TESS, fitted on the primates tree. One hundred episodes. Each estimate is the posterior median. The time unit is millions of years before the present.
+#' 
+#' @docType data
+#' 
+#' @usage data(primates_ebd_tess)
+#' 
+"primates_ebd_tess"
+
+#' TreePar Primates birth-death model
+#' 
+#' The results of a birth-death model in the R-package TreePar, fitted on the primates tree. The estimated model has two epochs, that are maximum-likelihood estimates. The time unit is millions of years before the present.
+#' 
+#' @docType data
+#' 
+#' @usage data(primates_ebd_treepar)
+#' 
+"primates_ebd_treepar"
