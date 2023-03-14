a0e592e2d169ab6ee9b0fda09dbf41339ac9fedb
Bjorn Tore Kopperud <kopperud@protonmail.com>
2023-03-14 14:06:51 +0100

    namespace

--- R/CRABS.R
+++ R/CRABS.R
@@ -7,17 +7,18 @@
 #'@importFrom magrittr %>%
 #'@importFrom latex2exp latex2exp
 #'@importFrom tidyr gather
-#'@importFrom stats approxfun rlnorm rgamma rcauchy rnorm time
+#'@importFrom stats approxfun rlnorm rgamma rcauchy rnorm time quantile rbeta
 #'@importFrom utils txtProgressBar setTxtProgressBar head tail read.table
 #'@importFrom ggplot2 ggplot aes scale_fill_gradient2 theme_classic ggtitle theme element_text scale_x_continuous 
 #'@importFrom ggplot2 geom_line scale_x_reverse scale_color_manual element_blank geom_hline geom_tile xlab ylim 
 #'@importFrom ggplot2 scale_fill_manual ylab labs element_rect facet_grid scale_linetype_manual scale_y_continuous geom_histogram
+#'@importFrom graphics matplot par
 #'@importFrom dplyr group_map bind_rows filter
 #'@importFrom patchwork plot_layout
 #'@importFrom colorspace sequential_hcl
 #'@importFrom ape branching.times
 #'@importFrom pracma fderiv
-#'@importFrom paletteer paletteer_c
+#'
 #' 
 #' 
 #' @section References:
