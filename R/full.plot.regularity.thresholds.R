97689b02a600fd959ac375199b3f84f5875c5705
Bjorn Tore Kopperud <kopperud@protonmail.com>
2023-10-23 15:36:38 +0200

    changes regularity plot

--- R/full.plot.regularity.thresholds.R
+++ R/full.plot.regularity.thresholds.R
@@ -1,3 +1,57 @@
+# Filter with increasing regularity thresholds
+regularity_filtering <- function(filtering_fraction, samples, samples_penalty){
+  eligible_models <- names(samples_penalty[samples_penalty<=quantile(samples_penalty, filtering_fraction)])
+  # take 50 samples among the models below the regularity threshold
+  n_samples <- min(length(eligible_models), 50)
+  filtered_samples <- samples[sample(eligible_models, n_samples)]
+  if (!("reference" %in% names(filtered_samples))){
+    filtered_samples[[1]] <- samples$reference
+    names(filtered_samples)[1] <- "reference"
+  }
+  class(filtered_samples) <- c("list", "CRABSset")
+  return(filtered_samples)
+}
+
+plot_rate <- function(samples_list, i, rate, xlab="time before present", 
+                      main="Congruent diversification histories"){
+  
+  palettes = c("lambda" = "Blues", 
+               "mu" = "orange", 
+               "delta" = "purple", 
+               "epsilon" = "Greens")
+  
+  ylabs = c("lambda" = "Speciation rate", 
+            "mu" = "Extinction rate", 
+            "delta" = "Net-diversification rate", 
+            "epsilon" = "Turnover rate")
+  
+  sample_i <- samples_list[[i]]
+  sample_i <- sample_i[sort(names(sample_i))]
+  
+  rate_min <- 0.0
+  rate_max <- 0.99
+  for (i in seq_along(samples_list)){
+    samples_set <- samples_list[[i]]
+    for (j in seq_along(samples_set)){
+      rate_min <- min(rate_min, min(get_rates(samples_set[[j]], rate = rate)))
+      rate_max <- max(rate_max, max(get_rates(samples_set[[j]], rate = rate)))
+    }
+  }
+  rate_max <- rate_max * 1.1
+  rates_matrix <- sapply(sample_i, get_rates, rate=rate)
+  
+  cols <- c(head(sequential_hcl(palette=palettes[rate], n=ncol(rates_matrix)),n=-1), "black")
+  matplot(sample_i$reference$times, rates_matrix, type="l", lty=c(rep(5,ncol(rates_matrix)-1),1), 
+          lwd=c(rep(1,ncol(rates_matrix)-1),1.5), 
+          col = cols, ylim=c(rate_min,rate_max), xlim=rev(range(sample_i$reference$times)),
+          xlab=xlab, ylab=ylabs[rate], main=main)
+}
+
+get_rates <- function(samples, rate) {
+  res <- samples[[rate]](samples$times)
+  return(res)
+}
+
 #' Plots the rate functions after filtering them according to a given penalty and predefined thresholds.
 #'
 #' @param samples A list of (congruent) CRABS models
@@ -10,6 +64,7 @@
 #' @export
 #' @examples
 #' data("primates_ebd")
+#' set.seed(123)
 #' 
 #' l <- approxfun(primates_ebd[["time"]], primates_ebd[["lambda"]])
 #' mu <- approxfun(primates_ebd[["time"]], primates_ebd[["mu"]])
@@ -26,7 +81,7 @@
 #' }
 #' 
 #' joint.samples <- sample.congruence.class(model = model, 
-#'                                          num.samples = 5000, 
+#'                                          num.samples = 100, 
 #'                                          rate.type = "joint", 
 #'                                          sample.joint.rates = sample.joint.rates)
 #' 
@@ -50,52 +105,9 @@ full.plot.regularity.thresholds <- function(samples,
     stop("Invalid \"penalty\"")
   }
   samples_penalty <- sapply(samples, penalty)
-  
-  # Filter with increasing regularity thresholds
-  regularity_filtering <- function(filtering_fraction, samples, samples_penalty){
-    # take 50 samples among the models below the regularity threshold
-    filtered_samples <- samples[sample(names(samples_penalty[samples_penalty<=quantile(samples_penalty, filtering_fraction)]), 50)]
-    if (!("reference" %in% names(filtered_samples))){
-      filtered_samples[[1]] <- samples$reference
-      names(filtered_samples)[1] <- "reference"
-    }
-    class(filtered_samples) <- c("list", "CRABSset")
-    return(filtered_samples)
-  }
+
   filtered_samples_list <- lapply(filtering_fractions, regularity_filtering, samples, samples_penalty)
-  
-  get_rates <- function(samples, rate) {
-    if (rate == "delta") return (samples$lambda(samples$times) - samples$mu(samples$times))
-    return(samples[[rate]](samples$times))
-  }
-  
-  plot_rate <- function(samples_list, i, rate, xlab="time before present", 
-                        main="Congruent diversification histories"){
-    
-    palettes = c("lambda" = "Blues", 
-                 "mu" = "orange", 
-                 "delta" = "purple", 
-                 "epsilon" = "Greens")
-    
-    ylabs = c("lambda" = "Speciation rate", 
-              "mu" = "Extinction rate", 
-              "delta" = "Net-diversification rate", 
-              "epsilon" = "Turnover rate")
-    
-    sample_i <- samples_list[[i]]
-    sample_i <- sample_i[sort(names(sample_i))]
-    rate_min <- min(min(sapply(samples_list, function(sample_i) sapply(sample_i, get_rates, rate=rate))), 0)
-    rate_max <- quantile(sapply(samples_list, function(sample_i) sapply(sample_i, get_rates, rate=rate)), 0.99)*1.1
-    rates_matrix <- sapply(sample_i, get_rates, rate=rate)
-    
-    #cols <- c(paletteer_c(palette=palettes[rate], n=ncol(rates_matrix)-1), "black")
-    cols <- c(head(sequential_hcl(palette=palettes[rate], n=ncol(rates_matrix)),n=-1), "black")
-    matplot(sample_i$reference$times, rates_matrix, type="l", lty=c(rep(5,ncol(rates_matrix)-1),1), 
-            lwd=c(rep(1,ncol(rates_matrix)-1),1.5), 
-            col = cols, ylim=c(rate_min,rate_max), xlim=rev(range(sample_i$reference$times)),
-            xlab=xlab, ylab=ylabs[rate], main=main)
-  }
-  
+
   lr = length(rates)
   lf = length(filtering_fractions)
   par(mfrow=c(lr,lf), mar=c(2.6, 3.6, 2, 0.6), mgp=c(1.5, 0.5, 0))
